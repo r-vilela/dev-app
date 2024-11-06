a6308ac88b7dae290f8ba12d695fd737284f9c40
Vilela <raulvilela29@gmail.com>
2024-11-05 22:04:00 -0400

    add: login page and routing

---
+++ app/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native"
+
+export default function Login() {
+    return (
+        <View style={styles.container}>
+            <View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }
+})
